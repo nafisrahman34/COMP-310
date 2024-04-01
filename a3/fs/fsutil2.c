58ca417457d6f0d447d1df39a37b4b3a53b9e37a
Nafis Rahman <nafisrahman18@gmail.com>
2024-04-01 08:15:33 -0400

    implemented copy_in and copy_out

--- a3/fs/fsutil2.c
+++ a3/fs/fsutil2.c
@@ -15,12 +15,59 @@
 #include <string.h>
 
 int copy_in(char *fname) {
-  // TODO
+  FILE *fp = fopen(fname, "r");
+  if(fp == NULL) return -1;
+  if(fseek(fp, 0, SEEK_END)<0){
+    fclose(fp);
+    return -1;
+  }
+  //obtain size of file on real hard drive
+  long size = ftell(fp);
+  rewind(fp);
+  //allocate space for buffer and read contents of file into buffer
+  char *buffer = malloc((size + 1)*sizeof(char));
+  memset(buffer, 0, size+1);
+  fread(buffer, size, 1, fp);
+  fclose(fp);
+  //check if space is available on shell disk for the file and create the file
+  long freespace = fsutil_freespace() * BLOCK_SECTOR_SIZE;
+  int status = size>freespace ? fsutil_create(fname, freespace) : fsutil_create(fname, size);
+  //raise FILE_CREATION_ERROR if file not successfully created
+  if(status == 0) return 9;
+  int bytes_written = fsutil_write(fname, buffer, size);
+  free(buffer);
+  //raise FILE_WRITE_ERROR if writing to file is unsuccessful otherwise if bytes written to shell hard drive is smaller than file size, print a message
+  if (bytes_written == -1) {
+    return 11;
+  } else if (bytes_written != size) {
+    printf("Warning: could only write %d out of %ld bytes (reached end of file)\n", bytes_written, size);
+  }
   return 0;
 }
 
 int copy_out(char *fname) {
-  // TODO
+  int size = fsutil_size(fname);
+  char* buffer = malloc((size+1)*sizeof(char));
+  memset(buffer, 0, size+1);
+  struct file *file_s = get_file_by_fname(fname);
+  if (file_s == NULL) {
+    file_s = filesys_open(fname);
+    if (file_s == NULL) {
+      return -1;
+    }
+    add_to_file_table(file_s, fname);
+  }
+  offset_t cur_offset = file_tell(file_s);
+  file_seek(file_s, 0);
+  fsutil_read(fname, buffer, size);
+  file_seek(file_s, cur_offset);
+  FILE* file = fopen(fname, "w");
+  if(file == NULL){
+    return 11;
+  }
+  fputs(buffer, file);
+  fclose(file);
+  free(buffer);
   return 0;
 }
 
